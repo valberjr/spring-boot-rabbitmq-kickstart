11d3d3ea9f1badfec7dcbd79d16ee2ae2ce4cd70
valberjr <3cifhzht@duck.com>
2022-08-18 18:00:39 -0300

    consumer

---
+++ rabbitmq-consumer/src/main/java/com/example/rabbitmq/QueueConsumer.java
@@ -0,0 +1,14 @@
+package com.example.rabbitmq;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueueConsumer {
+
+    @RabbitListener(queues = {"${queue.name}"})
+    public void receive(@Payload String fileBody) {
+        System.out.println("Message: " + fileBody);
+    }
+}
